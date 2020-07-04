f577dd09ea0bf2e772953458ef4ecb82e75246e1
sonnguyenece@gmail.com <sonnguyenece@gmail.com>
2020-07-05 03:06:28 +0700

    calculator Angular

--- PetInfo/src/app/app.module.ts
+++ PetInfo/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { PetComponentComponent } from './pet-component/pet-component.component';
 import {FormsModule} from "@angular/forms";
 import { FontSizeEditorComponent } from './font-size-editor/font-size-editor.component';
 import { CalculatorComponent } from './calculator/calculator.component';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PetComponentComponent,
     FontSizeEditorComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    ColorPickerComponent
   ],
     imports: [
         BrowserModule,
